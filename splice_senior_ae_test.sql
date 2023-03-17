219f722b853ba510c9e6a03625538aa3d7bbbefd
Laura Wade <wadelaura989@gmail.com>
2023-03-17 11:50:35 -0400

    Update splice_senior_ae_test.sql

--- splice_senior_ae_test.sql
+++ splice_senior_ae_test.sql
@@ -19,7 +19,7 @@ SELECT DATE_TRUNC(event_timestamp,HOUR) AS event_date_hour,
   COUNT(DISTINCT event_id) AS total_events
 FROM adjust_timestamps
 GROUP BY 1
-ORDER BY 1 DESC
+ORDER BY 1 DESC;
 
 /*
 2. Each event or hit may be associated with nested product-related fields, found in hits.product. 
@@ -28,11 +28,11 @@ We also want to make sure we're analyzing true user-actions (see hits.type) and 
 */
 
 SELECT 
-  products.v2ProductCategory as product_category,
+  products.v2ProductCategory AS product_category,
   COUNT(DISTINCT fullVisitorId) AS unique_users_clicked
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`,
   UNNEST(hits) AS hits,
-  UNNEST(hits.product) as products
+  UNNEST(hits.product) AS products
 WHERE hits.eventInfo.eventAction IN ('Quickview Click', 'Product Click', 'Promotion Click')
   AND hits.type IN ("TRANSACTION", "ITEM", "EVENT", "SOCIAL") --assuming PAGE, APPVIEW are view events and EXCEPTION is an error and not a true user event
 GROUP BY 1
@@ -59,26 +59,111 @@ SELECT
   session_had_purchase,
   COUNT(DISTINCT fullvisitorId || '_' || visitId) AS unique_add_to_cart_sessions
 FROM add_to_cart_sessions
-GROUP BY 1
+GROUP BY 1;
 
 /*
 4. Now, knowing how to determine sessions with purchases vs. sessions with abandoned carts, let's wrap this up by building a data set that we think contains useful features for a model that predicts if a session will ultimately end up with an abandoned cart or a successful purchase. In this case, feel free to explore the data and add any data you think might be meaningful. You should expand your final data set to pull from bigquery-public-data.google_analytics_sample.ga_sessions*, giving you more data to work with. Please provide a brief write up of the additional columns/features you've chosen and why you think they matter.
 */
 
+--For each day get the top 5 abandoned products by the 30 day rolling average of users who abandoned the product
+--We only need cart events for sessions that did not have a purchase
+CREATE OR REPLACE TABLE `test.top_5_abandoned_products_by_day` AS
+WITH add_to_cart AS (
+  SELECT 
+    TIMESTAMP_SECONDS(visitStartTime) AS visit_start_timestamp,
+    fullvisitorId,
+    visitId,
+    products.productSKU AS productSKU,
+    products.productQuantity AS productQuantity_added
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
+    UNNEST(hits) AS hits,
+    UNNEST(product) AS products
+  WHERE hits.eventInfo.eventAction = 'Add to Cart'
+    AND totals.transactions IS NULL
+  GROUP BY 1,2,3,4,5
+)
+, remove_from_cart AS (
+  SELECT 
+    TIMESTAMP_SECONDS(visitStartTime) AS visit_start_timestamp,
+    fullvisitorId,
+    visitId,
+    products.productSKU AS productSKU,
+    products.productQuantity AS productQuantity_removed
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
+    UNNEST(hits) AS hits,
+    UNNEST(product) AS products
+  WHERE hits.eventInfo.eventAction = 'Remove from Cart'
+    AND totals.transactions IS NULL
+  GROUP BY 1,2,3,4,5
+)
+, users_abandoned_by_date AS (
+  SELECT 
+    DATE(atc.visit_start_timestamp) AS visit_date,
+    atc.productSKU,
+    COUNT(DISTINCT CASE WHEN atc.productQuantity_added - COALESCE(rfc.productQuantity_removed, 0) > 0 THEN atc.fullvisitorID END) AS users_abandoned_product,
+  FROM add_to_cart atc
+  LEFT JOIN remove_from_cart rfc
+    ON atc.fullVisitorId = rfc.fullVisitorId
+    AND atc.visitId = rfc.visitId
+    AND atc.productSKU = rfc.productSKU
+  WHERE rfc.visitId IS NULL
+  GROUP BY 1, 2
+)
+, rolling_30_day AS (
+  SELECT visit_date,
+    productSKU,
+    AVG(users_abandoned_product) OVER(PARTITION BY productSKU ORDER BY visit_date ASC ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) as rolling_30_day_abandon_average
+  FROM users_abandoned_by_date
+)
+, rank_products AS (
+SELECT visit_date,
+  productSKU,
+  ROW_NUMBER() OVER(PARTITION BY visit_date ORDER BY rolling_30_day_abandon_average DESC) AS daily_abandoned_product_rank
+FROM rolling_30_day
+)
+SELECT *
+FROM rank_products
+WHERE daily_abandoned_product_rank <= 5;
+
+--Identify if a session add any of the top 5 abandoned items to their cart on that day
+CREATE OR REPLACE TABLE `test.visitor_session_has_top_5_abandoned_product` AS
+WITH add_to_cart AS (
+  SELECT 
+    TIMESTAMP_SECONDS(visitStartTime) AS visit_start_timestamp,
+    DATE(TIMESTAMP_SECONDS(visitStartTime)) AS visit_start_date,
+    fullvisitorId,
+    visitId,
+    products.productSKU as productSKU
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
+    UNNEST(hits) AS hits,
+    UNNEST(product) AS products
+  WHERE hits.eventInfo.eventAction = 'Add to Cart'
+  GROUP BY 1,2,3,4,5
+)
+SELECT atc.fullVisitorId,
+  atc.visitID,
+  LOGICAL_OR(top.productSKU IS NOT NULL) AS added_top_5_abandoned_product_to_cart,
+FROM add_to_cart atc
+LEFT JOIN test.top_5_abandoned_products_by_day top
+  ON atc.productSKU = top.productSKU
+  AND atc.visit_start_date = top.visit_date
+GROUP BY 1, 2;
+
+--Aggregate the final prediction dataset
+CREATE OR REPLACE TABLE `test.abandoned_cart_prediction_dataset` AS
 WITH add_to_cart_sessions AS (
   SELECT 
     fullVisitorId,
     visitId,
     visitStartTime,
-    device.deviceCategory as device_category,
-    trafficSource.medium as traffic_source_medium,
+    device.deviceCategory AS device_category,
+    trafficSource.medium AS traffic_source_medium,
     visitNumber = 1 AS is_first_time_visitor,
     totals.transactions AS total_transactions,
     totals.totalTransactionRevenue,
-    ROUND(totals.totalTransactionRevenue/1000000, 2) as total_transaction_revenue,
+    ROUND(totals.totalTransactionRevenue/1000000, 2) AS total_transaction_revenue,
     SUM(CASE WHEN hits.eventInfo.eventAction = 'Add to Cart' THEN 1 END) AS num_add_to_cart_actions,
     SUM(CASE WHEN hits.eventInfo.eventAction = 'Remove from Cart' THEN 1 END) AS num_remove_from_cart_actions,
-    SUM(CASE WHEN hits.eventInfo.eventAction = 'Add to Cart' THEN 1 END) - COALESCE(SUM(CASE WHEN hits.eventInfo.eventAction = 'Remove from Cart' THEN 1 END),0) AS items_in_cart,
     SUM(CASE WHEN hits.eventInfo.eventAction = 'Product Click' THEN 1 END) AS num_product_clicks,
     SUM(CASE WHEN hits.eventInfo.eventAction = 'Promotion Click' THEN 1 END) AS num_promotion_clicks,
     --calculating visitor level total transaction revenue prior to the current session
@@ -93,20 +178,25 @@ WITH add_to_cart_sessions AS (
   HAVING SUM(CASE WHEN hits.eventInfo.eventAction = 'Add to Cart' THEN 1 END) > 0 --only pull sessions with an add to cart event
 )
 SELECT 
-  fullVisitorId,
-  visitId,
-  TIMESTAMP_SECONDS(visitStartTime) as visit_start_timestamp,
+  acsd.fullVisitorId,
+  acsd.visitId,
+  TIMESTAMP_SECONDS(visitStartTime) AS visit_start_timestamp,
   EXTRACT(DAYOFWEEK FROM TIMESTAMP_SECONDS(visitStartTime)) AS visit_start_day_of_week,
   EXTRACT(HOUR FROM TIMESTAMP_SECONDS(visitStartTime)) AS visit_start_hour,
   device_category,
   traffic_source_medium,
   is_first_time_visitor,
-  items_in_cart,
+  num_add_to_cart_actions,
+  num_remove_from_cart_actions,
   num_product_clicks,
   num_promotion_clicks,
   cumulative_transaction_revenue,
-  first_visitID_with_purchase is not null and first_visitID_with_purchase <> visitID AS visitor_previously_purchased,
+  first_visitID_with_purchase IS NOT NULL AND first_visitID_with_purchase <> acsd.visitID AS visitor_previously_purchased,
+  ap.added_top_5_abandoned_product_to_cart,
   COALESCE(total_transactions > 0, FALSE) AS session_had_purchase
-from add_to_cart_sessions acsd
+FROM add_to_cart_sessions acsd
+LEFT JOIN test.visitor_session_has_top_5_abandoned_product ap
+  ON acsd.fullVisitorId = ap.fullVisitorId
+  AND acsd.visitId = ap.visitID
 WHERE is_first_session
 ;
